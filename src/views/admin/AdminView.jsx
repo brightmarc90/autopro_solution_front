05637f994455a24d77a5ad777b9304317a0a8f10
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-03-20 23:42:22 +0100

    Barre d'état de l'utilisateur

--- src/views/admin/AdminView.jsx
+++ src/views/admin/AdminView.jsx
@@ -1,63 +1,31 @@
-import { NavLink } from "react-router-dom"
+import SideNavbar from "../../components/admin/SideNavbar"
 
 const AdminView = () => {
     return (
         <div className="g-sidenav-show bg-gray-100">
             <div class="min-height-300 bg-primary position-absolute w-100"></div>
-            <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-            <div class="sidenav-header">
-                <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-                <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-                    <img src="/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo"/>
-                    <span class="ms-1 font-weight-bold">Garage V. Parrot</span>
-                </a>
-            </div>
-            <hr class="horizontal dark mt-0"/>
-            <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <NavLink className="nav-link" to="/admin">
-                            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-symbols-outlined text-primary text-lg opacity-10">construction</i>
-                            </div>
-                            <span class="nav-link-text ms-1">Services</span>
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink className="nav-link" to="/admin">
-                            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-symbols-outlined text-primary text-lg opacity-10">contact_mail</i>
-                            </div>
-                            <span class="nav-link-text ms-1">Contacts</span>
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink className="nav-link" to="/admin">
-                            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-symbols-outlined text-primary text-lg opacity-10">garage</i>
-                            </div>
-                            <span class="nav-link-text ms-1">Voitures</span>
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink className="nav-link" to="/admin">
-                            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-symbols-outlined text-primary text-lg opacity-10">person</i>
-                            </div>
-                            <span class="nav-link-text ms-1">Utilisateurs</span>
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink className="nav-link" to="/admin">
-                            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-symbols-outlined text-primary text-lg opacity-10">reviews</i>
-                            </div>
-                            <span class="nav-link-text ms-1">Avis clients</span>
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-            </aside>
+            <SideNavbar />
+            <main class="main-content position-relative border-radius-lg ">
+                <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+                    <div class="container-fluid py-1 px-3">
+                        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                            <ul class="navbar-nav  justify-content-end">
+                                <li class="nav-item d-flex align-items-center">
+                                    <div class="text-white font-weight-bold px-0">
+                                        <i class="fa fa-user me-sm-1"></i>
+                                        <span class="d-sm-inline d-none">Nom prénom</span>
+                                    </div>                         
+                                </li>
+                                <li class="nav-item d-flex align-items-center ms-4">
+                                    <div class="text-white px-0">
+                                        <span class="material-symbols-outlined pt-2">logout</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </main>
         </div>
     )
 }
