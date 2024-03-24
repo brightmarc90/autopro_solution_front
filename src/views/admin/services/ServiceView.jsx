cb1c9b79db218a1346fc822d1852576331822763
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-03-24 02:33:51 +0100

    Intégration du tableau de services

--- src/views/admin/services/ServiceView.jsx
+++ src/views/admin/services/ServiceView.jsx
@@ -1,6 +1,37 @@
 const ServiceView = () => {
     return (
-        <div>ServiceView</div>
+        <div className="row">
+            <div className="col-12">
+                <div className="card mb-4">
+                    <div className="card-header pb-0 d-flex justify-content-between align-items-center">
+                        <h6>Liste des services</h6>
+                        <div><button className="btn btn-primary">Nouveau service</button></div>
+                    </div>
+                    <div className="ccard-body px-0 pt-0 pb-2">
+                        <div className="table-responsive p-0">
+                            <table className="table align-items-center mb-0">
+                                <thead>
+                                    <tr>
+                                        <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">#</th>
+                                        <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nom</th>
+                                        <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Description</th>
+                                        <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Nom</td>
+                                        <td>description</td>
+                                        <td>edit, delete</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
